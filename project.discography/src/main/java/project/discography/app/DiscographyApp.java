6fac3120530ae9460328663feb427de91a4f92e6
MatuTia <matucci.mattia@gmail.com>
2023-09-21 12:36:55 +0200

    Configure App to use picocli

--- project.discography/src/main/java/project/discography/app/DiscographyApp.java
+++ project.discography/src/main/java/project/discography/app/DiscographyApp.java
@@ -1,14 +1,39 @@
 package project.discography.app;
 
 import java.awt.EventQueue;
+import java.util.concurrent.Callable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 import project.discography.view.swing.DiscographySwingView;
 
-public class DiscographyApp {
+@Command(mixinStandardHelpOptions = true)
+public class DiscographyApp implements Callable<Void> {
+
+	@Option(names = { "--mongo-host" }, description = "MongoDB host address")
+	private String mongoHost = "localhost";
+
+	@Option(names = { "--mongo-port" }, description = "MongoDB host port")
+	private int mongoPort = 27017;
+
+	@Option(names = { "--database-name" }, description = "Database name")
+	private String databaseName = "discography";
+
+	@Option(names = { "--album-collection-name" }, description = "Album collection name")
+	private String albumCollectionName = "album";
+
+	@Option(names = { "--musician-collection-name" }, description = "Musician collection name")
+	private String musicianCollectionName = "musician";
 
 	public static void main(String[] args) {
+		new CommandLine(new DiscographyApp()).execute(args);
+	}
+
+	@Override
+	public Void call() throws Exception {
 		EventQueue.invokeLater(() -> {
 			try {
 				DiscographySwingView frame = new DiscographySwingView();
@@ -17,6 +42,7 @@ public class DiscographyApp {
 				Logger.getLogger("DiscographyApp").log(Level.SEVERE, "Exception", e);
 			}
 		});
+		return null;
 	}
 
 }
